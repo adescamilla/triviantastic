89fa5bda1a2b581c7e2634825ad5d6c611112c4a
Andrew Escamilla <aescami6@stu.southtexascollege.edu>
2023-04-07 15:03:39 -0500

    Main page layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <RouterView />
+  <main>
+    <div class="card">
+      <RouterView />
+    </div>    
+  </main>
 </template>
+
+<style lang="postcss" scoped>
+main {
+  @apply flex h-screen items-center justify-center bg-gradient-to-tr from-blue-300 via-green-200 to-yellow-300;
+
+  & .card {
+    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
+  }
+}
+</style>

c9e2dc7cb56933e9103fbbca1fa42eee9334cd33
Andrew Escamilla <aescami6@stu.southtexascollege.edu>
2023-04-09 18:33:56 -0500

    added base title

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,21 +1,22 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 import useAPI from '@/composables/useAPI'
-
+import BaseTitle from '@/components/BaseTitle.vue'
 const api = useAPI()
 const categories = ref([])
-
 onMounted(async () => {
-   categories.value = await api.getCategories()
+  categories.value = await api.getCategories()
 })
 </script>
 
 <template>
-   <div class="brand">
-    <img class="logo" src="logo.svg" alt="logo" />
-    <h1 class="title">Triviantastic</h1>
-    <img class="logo" src="logo.svg" alt="logo" />
-  </div>
+  <!--  -->
+  <BaseTitle>
+    <template #logo>
+      <img src="logo.svg" alt="logo" />
+    </template>
+    Triviantastic
+  </BaseTitle>
   <div class="categories">
     <RouterLink v-for="category in categories" :key="category.id" :to="`/question/category/${category.id}`"
       class="category">
@@ -25,27 +26,13 @@ onMounted(async () => {
 </template>
 
 <style lang="postcss" scoped>
-.brand {
-   @apply flex items-center justify-center gap-4;
-
-   & .logo {
-      @apply h-16 w-16;
-   }
-
-   & .title {
-      @apply text-6xl font-thin uppercase tracking-widest text-slate-700
-   }
-}
-
 .categories {
-   @apply grid flex-grow grid-cols-4 gap-12;
-
-   & .category {
-       @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
-
-       &:hover {
-         @apply cursor-pointer border-red-500 bg-red-500 text-white;
-       }
-   }
+  @apply grid flex-grow grid-cols-4 gap-12;
+  & .category {
+    @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
+    &:hover {
+      @apply cursor-pointer border-red-500 bg-red-500 text-white;
+    }
+  }
 }
 </style>
